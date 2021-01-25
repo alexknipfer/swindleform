9e216bcd3268c9fe229a0ace28bfaef9772acb3d
alexknipfer <alexanderknipfer@gmail.com>
2021-01-25 17:36:07 -0500

    Add app config

---
+++ config/appConfig.ts
@@ -0,0 +1,26 @@
+const isServer = !!process.browser;
+
+function loadServerEnvVar(key: string) {
+  const value = process.env[key];
+
+  if (isServer && !value) {
+    throw new Error(`Environment variable ${key} is required on server`);
+  }
+
+  return value;
+}
+
+export const appConfig = {
+  db: {
+    connectionString: loadServerEnvVar('DATABASE_CONNECTION_STRING'),
+  },
+  mailgun: {
+    user: loadServerEnvVar('EMAIL_SERVER_USER'),
+    password: loadServerEnvVar('EMAIL_SERVER_PASSWORD'),
+    serverHost: loadServerEnvVar('EMAIL_SERVER_HOST'),
+    serverPort: +loadServerEnvVar('EMAIL_SERVER_PORT'),
+    emailFrom: loadServerEnvVar('EMAIL_FROM'),
+  },
+};
+
+export type AppConfig = typeof appConfig;
