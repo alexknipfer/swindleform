b65b42e859e654d5a075b5bb6d458e72d2af97d6
alexknipfer <alexanderknipfer@gmail.com>
2021-01-28 23:18:40 -0500

    Remove fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,6 @@ import { Heading } from '@chakra-ui/react';
 import WorkspacesLayout from '@/layouts/Workspaces';
 import withAuthentication from '@/hoc/withAuthentication';
 import Chakra from '@/components/Chakra';
-import { gql, useQuery } from '@apollo/client';
-import { User } from '@/models/user';
-import { initializeApollo } from '@/apollo/client';
-import { contextResolver } from '@/apollo/contextResolver';
-import { GetServerSideProps, GetStaticProps } from 'next';
-
-const UserQuery = gql`
-  query UserQuery {
-    user {
-      id
-      email
-      workspaces {
-        id
-        workspaceName
-        users
-      }
-    }
-  }
-`;
 
 const Workspaces: React.FC = () => {
   return (
@@ -31,21 +12,5 @@ const Workspaces: React.FC = () => {
     </Chakra>
   );
 };
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   // const received = await contextResolver(context);
-//   // console.log('RECEIVED: ', received);
-//   const apolloClient = initializeApollo();
-
-//   await apolloClient.query({
-//     query: UserQuery,
-//   });
-
-//   return {
-//     props: {
-//       initialApolloState: apolloClient.cache.extract(),
-//     },
-//   };
-// };
 
 export default withAuthentication(Workspaces);
