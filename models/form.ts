421da257c75ccd085b39ab82139f9cfc94f36692
alexknipfer <alexanderknipfer@gmail.com>
2021-02-03 00:18:55 -0500

    Update form model

--- models/form.ts
+++ models/form.ts
@@ -1,24 +1,11 @@
-import { Entity, SnapshotBase } from 'sourced';
 import * as uuid from 'uuid';
 
-export interface FormSnapshot extends SnapshotBase {
-  formName: string;
-}
+export class Form {
+  id = '';
+  name = '';
 
-export class Form extends Entity<Form, FormSnapshot> {
-  formName = '';
-  constructor(snapshot?: any, events?: any[]) {
-    super();
-
-    this.rehydrate(snapshot, events);
-  }
-
-  init() {
+  constructor(name?: string) {
     this.id = uuid.v4();
-  }
-
-  updateName(params: { name: string }) {
-    this.formName = params.name;
-    this.digest('updateName', params);
+    this.name = name || 'My form';
   }
 }
