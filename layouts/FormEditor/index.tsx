28f7eec289d7ec9a616ed90bfa9f7895de9d2bfb
alexknipfer <alexanderknipfer@gmail.com>
2021-02-13 21:11:43 -0500

    Move toolbar to form create route

--- layouts/FormEditor/index.tsx
+++ layouts/FormEditor/index.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react';
 import Nav from '@/components/Nav';
 import { Box, Flex } from '@chakra-ui/react';
-import Toolbar, { ToolType } from '@/layouts/FormEditor/components/Toolbar';
 
 const FormEditorLayout: React.FC = ({ children }) => {
-  const [selectedTool, setSelectedTool] = useState(ToolType.BUILDER);
-
   return (
     <Flex direction="column" h="full">
       <Nav />
       <Flex w="full" h="full" as="main">
-        <Toolbar selectedTool={selectedTool} onToolSelect={setSelectedTool} />
         <Box w="full">{children}</Box>
       </Flex>
     </Flex>
--- pages/form/[formId]/create.tsx
+++ pages/form/[formId]/create.tsx
@@ -1,13 +1,21 @@
-import FormEditorLayout from '@/layouts/FormEditor';
+import { Flex } from '@chakra-ui/react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import FormEditorLayout from '@/layouts/FormEditor';
+
+import Toolbar, { ToolType } from './components/Toolbar';
 
 const CreateForm: NextPage = () => {
   const { query } = useRouter();
+  const [selectedTool, setSelectedTool] = useState(ToolType.BUILDER);
 
   return (
     <FormEditorLayout>
-      <h3>Create form for: {query.formId} </h3>
+      <Flex h="full">
+        <Toolbar selectedTool={selectedTool} onToolSelect={setSelectedTool} />
+        <h3>Create form for: {query.formId} </h3>
+      </Flex>
     </FormEditorLayout>
   );
 };
